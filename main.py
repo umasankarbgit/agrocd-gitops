7914e60391e3fbfc77edeb099afba71cf11c7d16
umasankarbgit <49721840+umasankarbgit@users.noreply.github.com>
2026-04-05 11:37:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,12 @@
 from flask import Flask
 import os
 
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
+@app.route("/")
+def index():
+    return os.getenv("MESSAGE", "Hello from ArgoCD Kubernetes 🚀")
 
-    @app.get("/")
-    def index():
-        message = os.getenv(
-            "MESSAGE",
-            "Hello from ArgoCD running on Kubernetes 🚀"
-        )
-
-        return f"""
-        <html>
-            <head><title>Secure Python App</title></head>
-            <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
-                <h1 style="color: black;">{message}</h1>
-            </body>
-        </html>
-        """
-
-    @app.get("/health")
-    def health():
-        return {"status": "ok"}, 200
-
-    return app
-
-
-app = create_app()
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", "8000"))
-    app.run(host="0.0.0.0", port=port)
+@app.route("/health")
+def health():
+    return {"status": "ok"}, 200
